e5328062301814634c90d4b78ea762f35f74e03e
Samarendra Kumar (EXT-Nokia) <samarendra.kumar.ext@nokia.com>
2025-07-06 21:30:19 +0530

    new rg added

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ module "azurerm_resource_group" {
     rg_location = "west europe"
 }
  
+ module "azurerm_resource_group" {
+    source= "../modules/azurerm_resource_group"
+    rg_name = "samar1"
+    rg_location = "central india"
+}
  module "azurerm_mssql_database" {
     source = "../modules/azurerm_sequal_database" 
     db_name = "samaridhi"
