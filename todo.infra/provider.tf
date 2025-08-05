38bb279959f57be09a7e48585aa65c9fb6c5fd83
samarendra12 <kumarsamarendra1@gmail.com>
2025-08-06 00:36:11 +0530

    Update provider.tf

--- todo.infra/provider.tf
+++ todo.infra/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.32.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  =  "sagar"
+    storage_account_name = "4204204"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "gautam"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "group.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  } 
 }
 
 provider "azurerm" {
